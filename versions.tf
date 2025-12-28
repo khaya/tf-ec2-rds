69b3eafab12046b4fa90b2becfa25d5dfe94cbe8
khaya <khayeni@gmail.com>
2025-12-28 12:07:40 +0200

    addition of backend

--- versions.tf
+++ versions.tf
@@ -1,6 +1,16 @@
 terraform {
   required_version = ">= 1.0"
 
+  backend "s3" {
+    bucket  = "tf-ec2-rds-state-bucket-2025"
+    key     = "tf-ec2-rds-state"
+    region  = "af-south-1"
+    encrypt = true
+    
+    # Optional: Enable state locking with DynamoDB
+    # dynamodb_table = "terraform-state-locks"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
